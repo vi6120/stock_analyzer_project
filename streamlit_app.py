16945911b48b55fe22ca1795b69860a6b525c987
vi6120 <116484203+vi6120@users.noreply.github.com>
2025-09-15 12:36:40 +0200

    updating the timer for news

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,7 +125,7 @@ st.markdown("""
     }
     .news-item {
         display: inline-block;
-        animation: scroll 30s linear infinite;
+        animation: scroll 05s linear infinite;
         padding-left: 120px;
         padding-right: 50px;
         font-weight: 500;
